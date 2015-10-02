8e61219edd156d7ff9e9319194ec4ac5824a1a15
fuyh <fuyh@asto-inc.com>
2015-10-03 00:06:32 +0800

    added libs

--- src/android/BaiduLocationPlugin.java
+++ src/android/BaiduLocationPlugin.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
@@ -17,11 +19,27 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.net.Uri;
 import android.util.Log;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.BDNotifyListener;//假如用到位置提醒功能，需要import该类
 
 public class BaiduLocationPlugin extends CordovaPlugin {
     private static final String TAG = "baidulocation";
     
     public BaiduLocationPlugin() {
+    }
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
     }
 
     /**
