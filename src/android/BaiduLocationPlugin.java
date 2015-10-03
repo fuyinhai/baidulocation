18c62dd465a244644f3b2266e8d8058111d035c3
fuyh <fuyh@asto-inc.com>
2015-10-03 13:12:00 +0800

    corrected the code

--- src/android/BaiduLocationPlugin.java
+++ src/android/BaiduLocationPlugin.java
@@ -8,6 +8,7 @@ import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.util.Log;
 import com.baidu.location.BDLocation;
@@ -22,7 +23,7 @@ import com.baidu.location.LocationClientOption.LocationMode;
 public class BaiduLocationPlugin extends CordovaPlugin  implements BDLocationListener {
     private static final String TAG = "baidulocation";
     private LocationClient mLocationClient = null;
-    private String locationInfo = "" ;
+    JSONObject addrInfo = new JSONObject();
 
     /**
      * Sets the context of the Command. This can then be used to do things like
@@ -33,14 +34,10 @@ public class BaiduLocationPlugin extends CordovaPlugin  implements BDLocationLis
      */
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        initLocation();
-    }
-
-    private void initLocation(){
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationMode.Battery_Saving);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span=1000;
+        int span=1000*60; // locate address per minute
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(false);//可选，默认false,设置是否使用gps
@@ -51,12 +48,10 @@ public class BaiduLocationPlugin extends CordovaPlugin  implements BDLocationLis
         mLocationClient = new LocationClient(cordova.getActivity().getApplicationContext());
         mLocationClient.setLocOption(option);
         mLocationClient.registerLocationListener(this);
-        if (!mLocationClient.isStarted()) {
-            mLocationClient.start();
-        }
+        mLocationClient.start();
     }
 
-    public void onStop() {
+    public void onDestroy() {
         if (mLocationClient != null && mLocationClient.isStarted()) {
             mLocationClient.stop();
         }
@@ -64,52 +59,35 @@ public class BaiduLocationPlugin extends CordovaPlugin  implements BDLocationLis
 
     @Override
     public void onReceiveLocation(BDLocation location) {
-        StringBuffer sb = new StringBuffer(256);
-        sb.append("time : ");
-        sb.append(location.getTime());
-        sb.append("\nerror code : ");
-        sb.append(location.getLocType());
-        sb.append("\nlatitude : ");
-        sb.append(location.getLatitude());
-        sb.append("\nlontitude : ");
-        sb.append(location.getLongitude());
-        sb.append("\nradius : ");
-        sb.append(location.getRadius());
-        if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果
-            sb.append("\nspeed : ");
-            sb.append(location.getSpeed());// 单位：公里每小时
-            sb.append("\nsatellite : ");
-            sb.append(location.getSatelliteNumber());
-            sb.append("\nheight : ");
-            sb.append(location.getAltitude());// 单位：米
-            sb.append("\ndirection : ");
-            sb.append(location.getDirection());// 单位度
-            sb.append("\naddr : ");
-            sb.append(location.getAddrStr());
-            sb.append("\ndescribe : ");
-            sb.append("gps定位成功");
-        } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){// 网络定位结果
-            sb.append("\naddr : ");
-            sb.append(location.getAddrStr());
-            //运营商信息
-            sb.append("\noperationers : ");
-            sb.append(location.getOperators());
-            sb.append("\ndescribe : ");
-            sb.append("网络定位成功");
-        } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-            sb.append("\ndescribe : ");
-            sb.append("离线定位成功，离线定位结果也是有效的");
-        } else if (location.getLocType() == BDLocation.TypeServerError) {
-            sb.append("\ndescribe : ");
-            sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-        } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-            sb.append("\ndescribe : ");
-            sb.append("网络不同导致定位失败，请检查网络是否通畅");
-        } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-            sb.append("\ndescribe : ");
-            sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+        try {
+            addrInfo.put("time"     , location.getTime());
+            addrInfo.put("errCode"  , location.getLocType());
+            addrInfo.put("latitude" , location.getLatitude());
+            addrInfo.put("lontitude", location.getLongitude());
+            addrInfo.put("radius"   , location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果
+                addrInfo.put("speed", location.getSpeed());
+                addrInfo.put("satellite",location.getSatelliteNumber());
+                addrInfo.put("height", location.getAltitude());// 单位：米
+                addrInfo.put("direction", location.getDirection());// 单位度
+                addrInfo.put("addr", location.getAddrStr());
+                addrInfo.put("describe", "gps定位成功");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){// 网络定位结果
+                addrInfo.put("addr", location.getAddrStr());
+                addrInfo.put("operationers", location.getOperators());
+                addrInfo.put("describe", "网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                addrInfo.put("describe", "离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                addrInfo.put("describe", "服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                addrInfo.put("describe", "网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                addrInfo.put("describe", "无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+        } catch (JSONException e) {
+            Log.w(TAG,e);
         }
-        locationInfo = sb.toString();
     }
 
     /**
@@ -119,7 +97,7 @@ public class BaiduLocationPlugin extends CordovaPlugin  implements BDLocationLis
     public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
         try {
             if ("getLocation".equals(action)) {
-                callbackContext.success(locationInfo);
+                callbackContext.success(addrInfo);
                 return true;
             } else {
                 Log.w(TAG, "Invalid action passed: " + action);
